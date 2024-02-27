6cf30ab13313fb3362003b83a16c58bc6cec53dc
Muhammad Anas <151528713+anas152@users.noreply.github.com>
2024-02-27 13:30:18 +0500

    Add files via upload

---
+++ java script assignment 1/app.js
@@ -0,0 +1,86 @@
+// chapter no 1 
+
+// Q no 01
+
+// var abc =  "Error! plz Enter a Valid password"
+// alert (abc)
+
+// Q no 02
+
+// var Q2 = "Welcome to JS land \n Happy Coding!"
+// alert (Q2);
+
+// Q no 03
+
+// var Q3 = "Welcone to JS land...."
+// alert (Q3)
+
+// Q no 04
+
+// var Q4 = "Happy Coding ! \n Prevent This Page From Creating Additional Dialogs "
+// alert (Q4)
+
+// Q no  05
+
+// var Q5 = ( "Hello... I Can Run JS Through My web Browser's Console ")
+// alert (Q5)
+
+// =======CHAPTER NO 02 =====
+
+// ----- Q1 -----
+
+// var userName = "Muhammad Anas"
+// alert (userName)
+// ----- Q2 -----
+
+// var message="Hello World";
+// alert(message);
+//  -----  Q3 ----
+
+// var userName = "Muhammad Anas"
+// alert (userName);
+
+// var age = "18 year old "
+// alert (age);
+
+// var course =  "Web and devoplepment "
+// alert (course)
+
+// ----- Q4 -----
+// var dish = "Pizza \n izza \n zza \n za \n a "
+// alert (dish)
+
+// -----  Q5 ------
+// var email = "anassoomro775@gmail.com";
+// alert (email);
+
+// ---- Q7 ------
+// var book = "A smarter way to learn JavaScript"
+// alert (book)
+
+// -------Q8-----
+// var browser = "Write a script to display this in browser through JS" 
+// document.write (browser)
+
+// ------Q9----
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert (design)
+
+// =========CHAPTER NO 03 =======
+
+// -------Q1 ------
+
+// var age = "I Am 18 Year Old";
+// alert (age);
+
+// ------Q2----
+// var visited = "You have visited this site 14 times "
+// alert (visited);
+// -------- Q3 ----
+// var showOnPage1 = "My Birth Day is 2005 \n";
+// document.write (showOnPage1)     
+// var showOnPage = "Data type of my ddecleard is number";
+// document.write (showOnPage) 
+
+// ------Q4-----
+
